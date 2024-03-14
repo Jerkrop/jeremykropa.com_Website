cf05a2aa470f916a3ac57f1590ba947004378409
Jerkrop <arkedi487@gmail.com>
2024-03-14 12:31:05 -0400

    changed the header so you can go back to home page

--- resume.php
+++ resume.php
@@ -15,13 +15,12 @@
           <a href="register_form.php" class="btn">Register</a>
               <a href="contactus.html" class="btn">Contact Me</a>
               <a href="resume.php" class="btn">Resume</a>
-              <?php  if (!isset($_SESSION['user_name'])) : ?><a href="login_form.php" class="btn">Login</a><?php endif ?>
-         <?php  if (isset($_SESSION['user_name'])) : ?> <a href="logout.php" class="btn">Logout</a><?php endif ?>
+                <a href="index.php" class="btn">Home</a>
          </div> 
          </div>
          <div class="container">
          <div class="content">
-
+         </br>
    <h1>Jeremy Kropa <h1>
     <p>Email me at: jerkrop@jeremykropa.com  </p>
 <h3>EDUCATION:</h3>
