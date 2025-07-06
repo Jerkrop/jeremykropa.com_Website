356e34006f299b0619c5c2f6afd42ce206fa511d
Jerkrop <arkedi487@gmail.com>
2025-07-06 19:58:12 -0400

    added code for icon to make them appear #1

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,5 @@
+<body>
+
 <link rel="stylesheet" href="css/style.css">
 
 <div class="footer_bottom">
@@ -9,3 +11,8 @@
          
 </div>
 </div>
+
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+</body>
