99625260c75d04f1700da028a96594c4edd7e445
Jerkrop <arkedi487@gmail.com>
2025-07-06 19:55:32 -0400

    attempt #3 to add icons to footer

--- includes/footer.php
+++ includes/footer.php
@@ -3,8 +3,8 @@
 <div class="footer_bottom">
    <a href="index.php" class="logo">Jeremy Kropa</a>         
 <div class="footer-right">
-<i class="fa-brands fa-github" style="color:white;"><a href="https://github.com/Jerkrop" class="btn"></a></i>
-<i class="fa-brands fa-linkedin" style="color:white;"><a href="https://www.linkedin.com/in/jeremy-kropa" class="btn" ></a></i>
+<ion-icon name="logo-github"><a href="https://github.com/Jerkrop" class="btn"></a></ion-icon>
+<ion-icon name="logo-linkedin"><a href="https://www.linkedin.com/in/jeremy-kropa" class="btn" ></a></ion-icon>
 
          
 </div>
