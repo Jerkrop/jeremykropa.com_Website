a96701c0ff44cbe731ffab27a47c1231852d76af
Jerkrop <arkedi487@gmail.com>
2025-07-06 20:16:18 -0400

    bootstrap icon #4

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,9 @@
 <head>
 <link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
-</head>
+<link href=
+"https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" 
+          rel="stylesheet" />
+   </head>
 <div class="footer_bottom">
    <a href="index.php" class="logo">Jeremy Kropa</a>         
 <div class="footer-right">
