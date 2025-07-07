f7e59c32006dd169bda17637229c1344c9305534
Jerkrop <arkedi487@gmail.com>
2025-07-06 20:14:19 -0400

    bootstrap icon #3

--- includes/footer.php
+++ includes/footer.php
@@ -1,14 +1,12 @@
-
-
+<head>
 <link rel="stylesheet" href="css/style.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
-
-
+</head>
 <div class="footer_bottom">
    <a href="index.php" class="logo">Jeremy Kropa</a>         
 <div class="footer-right">
-<i class="bi bi-github" href="https://github.com/Jerkrop" class="btn"></i>
-<i class="bi bi-linkedin" href="https://www.linkedin.com/in/jeremy-kropa" class="btn"></i>
+<i class="bi bi-github"></i>
+<i class="bi bi-linkedin"></i>
 
          
 </div>
