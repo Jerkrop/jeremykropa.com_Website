aaee48fe5fee4f9af39b37c5420ab25a2dd7df7c
Jerkrop <arkedi487@gmail.com>
2026-09-11 00:25:25 -0400

    Attempt

--- config.php
+++ config.php
@@ -4,5 +4,5 @@ $conn = mysqli_connect('localhost','u822265912_Jerkrop12','Meegee12*','u82226591
 
 // reCAPTCHA Configuration
 define('RECAPTCHA_SITE_KEY', '6LfDWHQpAAAAAKc_LkIKftO0Hz3dkPp_BXmVxJ7F');
-define('RECAPTCHA_SECRET_KEY', 'YOUR_ACTUAL_SECRET_KEY_HERE');
+define('RECAPTCHA_SECRET_KEY', '6LfDWHQpAAAAADm3mPAkYfk5Ic5jUccPBFVxX4FM');
 ?>
